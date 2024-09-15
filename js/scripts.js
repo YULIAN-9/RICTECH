b84d460644688e74797ca80860344ae46010c882
yulian-9 <julian.9521@hotmail.com>
2024-09-15 11:26:05 -0500

    Carga Proyecto

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Reducir barra de navegacion
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+ 
+    navbarShrink();
+
+    // Reducir barra de navegacion cuando se desplaza en la pagina
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activa Bootstrap scrollspy elmento de navegacion principal
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Contrae la barra de navegacion responsiva cuando el conmutador esta visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
